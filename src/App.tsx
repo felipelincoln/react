d2751f298d580d65c2e5c7a21d2e30a224dfecda
Felipe Lincoln <mail.felipelincoln@gmail.com>
2023-09-09 09:50:14 -0300

    add connect button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import './App.css';
+import { WagmiConfig, createConfig, mainnet, useEnsName } from 'wagmi';
+import { createPublicClient, http } from 'viem';
+import { useAccount } from 'wagmi';
+import ConnectButton from './components/ConnectButton';
 
-interface a {}
+const config = createConfig({
+  autoConnect: true,
+  publicClient: createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  }),
+});
+
+function Profile() {
+  const { address, isConnected } = useAccount();
+  const { data: ensName } = useEnsName({ address });
+
+  if (isConnected) {
+    return <div>{ensName ?? address}</div>;
+  }
+}
 
 function App() {
   return (
-    <>
-      <p>Button to connect wallet</p>
+    <WagmiConfig config={config}>
+      <Profile />
+      <ConnectButton />
       <p>User's nft list</p>
       <p>User's ETH balance list</p>
       <p>Button to create order</p>
       <br />
       <p>List of all orders</p>
-    </>
+    </WagmiConfig>
   );
 }
 
