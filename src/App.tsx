3c3b90d71845b575aadaceaf8e45efe3578ba9c4
Felipe Lincoln <mail.felipelincoln@gmail.com>
2023-09-16 11:07:36 -0300

    wire up with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css';
-import { WagmiConfig, createConfig, mainnet, useEnsName } from 'wagmi';
+import { WagmiConfig, createConfig, mainnet, useBalance, useEnsName } from 'wagmi';
 import { createPublicClient, http } from 'viem';
 import { useAccount } from 'wagmi';
 import ConnectButton from './components/ConnectButton';
+import { useEffect, useState } from 'react';
 
 const config = createConfig({
   autoConnect: true,
@@ -12,12 +13,35 @@ const config = createConfig({
   }),
 });
 
+interface NFT {
+  tokenId: string;
+  thumbnail?: string;
+}
+
 function Profile() {
   const { address, isConnected } = useAccount();
   const { data: ensName } = useEnsName({ address });
+  const [nfts, setNfts] = useState<NFT[]>([]);
+  const { data: balance } = useBalance({ address });
+
+  useEffect(() => {
+    if (!address) return;
+
+    fetch('http://localhost:3000/nfts/0x960b7a6bcd451c9968473f7bbfd9be826efd549a/' + address).then(
+      (res) => res.json().then(({ data }) => setNfts(data)),
+    );
+  }, [address]);
+
+  const listNfts = nfts.map((nft) => <img src={nft.thumbnail} alt={nft.tokenId} />);
 
   if (isConnected) {
-    return <div>{ensName ?? address}</div>;
+    return (
+      <>
+        <div>{ensName ?? address}</div>
+        <div>{balance?.formatted ?? 0} ETH</div>
+        <div>{listNfts}</div>
+      </>
+    );
   }
 }
 
@@ -26,8 +50,6 @@ function App() {
     <WagmiConfig config={config}>
       <Profile />
       <ConnectButton />
-      <p>User's nft list</p>
-      <p>User's ETH balance list</p>
       <p>Button to create order</p>
       <br />
       <p>List of all orders</p>
