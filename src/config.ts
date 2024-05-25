49a6f45ed76f11dfc7806476dd91f2e171256c1b
Felipe Lincoln <mail.felipelincoln@gmail.com>
2024-05-25 16:42:27 -0300

    update fee recipient

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const config = {
     },
   },
   fee: {
-    recipient: '0x9F1063848b32D7c28C4144c8Eb81B6597C8f961D',
+    recipient: '0x88FE6ED81c6fb5620265B227574ea8e0C8d08E9c',
     amount: '5000000000000000', // 0.005 ETH
   },
 };
