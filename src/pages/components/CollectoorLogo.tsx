5c7b90873acb68296c3881aca6099793bef1a3e1
Felipe Lincoln <mail.felipelincoln@gmail.com>
2024-05-31 20:47:26 -0300

    change logo

--- src/pages/components/CollectoorLogo.tsx
+++ src/pages/components/CollectoorLogo.tsx
@@ -1,41 +1,5 @@
+import logo from '../../assets/logo.svg';
+
 export function CollectoorLogo() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="64"
-      height="64"
-      color="rgb(24 24 27)"
-      fill="rgb(228 228 231)"
-    >
-      <path
-        d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
-        stroke="currentColor"
-        strokeWidth="1"
-        strokeLinejoin="round"
-      />
-      <g transform="translate(12, 12) scale(0.5) translate(-12, -12)">
-        <path
-          d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-          stroke="currentColor"
-          strokeWidth="1.5"
-        />
-        <path
-          d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-          stroke="currentColor"
-          strokeWidth="1.5"
-        />
-        <path
-          d="M15 13C15 14.6569 13.6569 16 12 16C10.3431 16 9 14.6569 9 13C9 11.3431 10.3431 10 12 10C13.6569 10 15 11.3431 15 13Z"
-          stroke="currentColor"
-          strokeWidth="1.5"
-        />
-        <path
-          d="M2 11C4.59628 12.0036 6.85307 12.668 8.99803 12.993M22 11.0031C19.4075 12.0125 17.152 12.6781 15.0021 13"
-          stroke="currentColor"
-          strokeWidth="1.5"
-        />
-      </g>
-    </svg>
-  );
+  return <img draggable="false" src={logo} />;
 }
