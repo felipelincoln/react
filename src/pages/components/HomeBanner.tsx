c0425d92c1a9a18c57854de96738b6b2abe1d82b
Felipe Lincoln <mail.felipelincoln@gmail.com>
2024-07-03 07:41:54 -0300

    homepage

--- src/pages/components/HomeBanner.tsx
+++ src/pages/components/HomeBanner.tsx
@@ -9,33 +9,40 @@ export function HomeBanner() {
   const [error, setError] = useState<string | undefined>();
 
   return (
-    <div className="max-w-screen-lg w-full mx-auto">
-      <div className="p-8 flex flex-col gap-8 items-center">
-        <h1 className="font-bold text-4xl">The marketplace to swap NFTs</h1>
-        <div className="flex flex-col flex-grow justify-end items-center gap-4">
-          <div className="flex gap-2">
-            <Input
-              placeholder="NFT contract"
-              onChange={(e) => {
-                setContract(e.target.value);
-                setError(undefined);
-              }}
-              value={contract}
-            />
-            <Button
-              onClick={() => {
-                if (isAddress(contract)) {
-                  navigate(`/c/${contract}`);
-                  return;
-                }
-
-                setError('Invalid contract address');
-              }}
-            >
-              Go
-            </Button>
+    <div className="bg-zinc-900 py-32 box-content border-b-2 border-zinc-800">
+      <div className="max-w-screen-lg w-full mx-auto">
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="font-bold text-5xl">The marketplace to swap NFTs</h1>
+            <h2 className="text-2xl text-zinc-400">
+              Create and fulfill trait-based listings using the OpenSea smart contract
+            </h2>
           </div>
-          {error && <div className="text-red-400">{error}</div>}
+          <div className="flex flex-col flex-grow justify-end items-center gap-4">
+            <div className="flex gap-2 hidden">
+              <Input
+                placeholder="NFT contract"
+                onChange={(e) => {
+                  setContract(e.target.value);
+                  setError(undefined);
+                }}
+                value={contract}
+              />
+              <Button
+                onClick={() => {
+                  if (isAddress(contract)) {
+                    navigate(`/c/${contract}`);
+                    return;
+                  }
+
+                  setError('Invalid contract address');
+                }}
+              >
+                Go
+              </Button>
+            </div>
+            {error && <div className="text-red-400">{error}</div>}
+          </div>
         </div>
       </div>
     </div>
