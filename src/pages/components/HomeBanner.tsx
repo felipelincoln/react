8ef737f9abc807444ab5ddad50ef4b04648d9ce9
Felipe Lincoln <mail.felipelincoln@gmail.com>
2024-07-08 14:42:02 -0300

    align

--- src/pages/components/HomeBanner.tsx
+++ src/pages/components/HomeBanner.tsx
@@ -56,10 +56,10 @@ function ExploreSection() {
   if (connectStatus === 'error' || connectStatus === 'idle') {
     return (
       <div
-        className="text-xl bg-cyan-400 font-bold rounded px-8 py-2 text-zinc-950 cursor-pointer"
+        className="text-xl content-center bg-cyan-400 font-bold rounded px-8 py-2 text-zinc-950 cursor-pointer"
         onClick={() => connect({ connector: injected(), chainId })}
       >
-        <span className="align-middle">Explore</span>
+        Explore
       </div>
     );
   }
